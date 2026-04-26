ca8ff4b46b3ef57867d99dd5c5067a01849a06f5
andreasandersson192-png <andreas.andersson192@gmail.com>
2026-04-26 12:27:35 +0200

    Menu.cs
    Jag har skrivit en kommentar i word dokumment på skolans sida. På engelska när jag jobbat och på svenska när jag va klar och lite hur jag tänkt. Gör jag något fel nu vid uppladdning så ber jag om ursäkt ! :)

---
+++ LibraryCodeGroupProject/LibraryLogic .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// This file is caling from menu and having the logic for the library management system. We will put all our logic for adding, removing and viewing books, customers and loans here. We will also have some helper methods for validating input and other common tasks.
+
+namespace LibraryCodeGroupProject
+{
+    public class LibraryLogic 
+    { 
+        private List<Book> books;
+        private List<Customer> customers;
+        
+        public LibraryLogic()
+        {
+            books = new List<Book>();
+            customers = new List<Customer>();
+        }
+        public list <Book> GetBooks()
+        {
+            return books;
+        }
+        public List<Customer> GetCustomers()
+        {
+            return customers;
+        }
+
+    }
+}
+// The logic is not complete yet, we will add methods for adding, removing and viewing books, customers and loans. We will also add some helper methods for validating input and other common tasks.
+// is going to be more complete in the future, we will add more methods and logic as we go along. We will also add some error handling and validation to make sure the program runs smoothly.
---
+++ LibraryCodeGroupProject/Menu.cs
@@ -0,0 +1,96 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCodeGroupProject
+{
+    public class Menu
+    {
+
+        public void DisplayMenu()
+        {
+
+            Console.WriteLine("Welcome to the Library Management System!");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Books");
+            Console.WriteLine("2. Customers");
+            Console.WriteLine("3. Loans");
+            Console.WriteLine("4. Exit");
+            string choice = Console.ReadLine();
+
+
+
+
+            switch (choice)
+            {
+                case "1":
+                    ShowBookMenu();
+                    break;
+                case "2":
+                    ShowCustomerMenu();
+                    break;
+                case "3":
+                    ShowLoanMenu();
+                    break;
+                case "4":
+                    ShowExit();
+                    Console.WriteLine("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+        
+
+
+        // Here we put all our diffrent menu options for Books, Customers and Loans. We also have an option to exit the program.
+private void ShowBookMenu()
+        {
+
+            Console.WriteLine("Books Menu:");
+            Console.WriteLine("1. Add Book");
+            Console.WriteLine("2. Remove Book");
+            Console.WriteLine("3. View Books");
+            Console.WriteLine("4. Back to Main Menu");
+            string bookChoice = Console.ReadLine();
+    // Handle book menu choices here
+
+
+private void ShowCustomerMenu()
+        {
+            Console.WriteLine("Customers Menu:");
+            Console.WriteLine("1. Add Customer");
+            Console.WriteLine("2. Remove Customer");
+            Console.WriteLine("3. View Customers");
+            Console.WriteLine("4. Back to Main Menu");
+            string customerChoice = Console.ReadLine();
+    // Handle customer menu choices here
+
+
+     private void ShowLoanMenu()
+        {
+            Console.WriteLine("Loans Menu:");
+            Console.WriteLine("1. Add Loan");
+            Console.WriteLine("2. Remove Loan");
+            Console.WriteLine("3. View Loans");
+            Console.WriteLine("4. Back to Main Menu");
+            string loanChoice = Console.ReadLine();
+    // Handle loan menu choices here
+
+
+            // Handle exit choice here
+     private void ShowExit()
+        {
+            Console.WriteLine("Exiting the program. Goodbye!");
+        }
+    }
+}
+
+// The menu file is not complete. Now i going to do the logic in the LibraryLogic file and then come back to the menu to connect it to the logic. I want to do this because i want to have a clear separation between the menu and the logic. This way we can easily change the menu without affecting the logic and vice versa.
+// Please add your opinons in the menu file.
