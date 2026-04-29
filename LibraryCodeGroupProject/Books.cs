07ceceac8bf00cf97513ed2986aaabe6eed01d0b
Ellatrix <ellatrix004@gmail.com>
2026-04-29 15:24:15 +0200

    Skapat Books.cs

---
+++ LibraryCodeGroupProject/Books.cs
@@ -0,0 +1,14 @@
+﻿Namespace LibraryCodeGroupProject
+{
+    public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public bool IsAvailable { get; set; }
+
+    {
+
+}
+// The logic is not complete yet
+
