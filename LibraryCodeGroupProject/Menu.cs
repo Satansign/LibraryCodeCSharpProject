d83b949e26085a2123960226510a566195fbe960
andreasandersson192-png <andreas.andersson192@gmail.com>
2026-05-04 11:06:32 +0200

    2026-05-04
    Jag har lagt till lite mer struktur i menu filerna. så när man trycker in på så kommer vi vidare till rätt under menu sen har jag påbörjat hur vi lägger till böcker och så. VI kommer behöva ändra namn och kanske lägga till saker som ISBN och så vidare men det pratar vi om mer när iv har möte.

--- LibraryCodeGroupProject/Menu.cs
+++ LibraryCodeGroupProject/Menu.cs
@@ -11,41 +11,44 @@ namespace LibraryCodeGroupProject
 {
     public class Menu
     {
- 
+
 
         public void DisplayMenu()
         {
-
-            Console.WriteLine("Welcome to the Library Management System!");
-            Console.WriteLine("Please select an option:");
-            Console.WriteLine("1. Books");
-            Console.WriteLine("2. Customers");
-            Console.WriteLine("3. Loans");
-            Console.WriteLine("4. Exit");
-            string choice = Console.ReadLine();
+            
 
 
+                Console.WriteLine("Welcome to the Library Management System!");
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Books");
+                Console.WriteLine("2. Customers");
+                Console.WriteLine("3. Loans");
+                Console.WriteLine("4. Exit");
+                string choice = Console.ReadLine();
 
 
-            switch (choice)
-            {
-                case "1":
-                    ShowBookMenu();
-                    break;
-                case "2":
-                    ShowCustomerMenu();
-                    break;
-                case "3":
-                    ShowLoanMenu();
-                    break;
-                case "4":
-                    ShowExit();
-                    Console.WriteLine("Exiting the program. Goodbye!");
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please try again.");
-                    break;
-            }
+
+
+                switch (choice)
+                {
+                    case "1":
+                        ShowBookMenu();
+                        break;
+                    case "2":
+                        ShowCustomerMenu();
+                        break;
+                    case "3":
+                        ShowLoanMenu();
+                        break;
+                    case "4":
+                        ShowExit();
+                        Console.WriteLine("Exiting the program. Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            
         }
     
         
@@ -60,8 +63,27 @@ namespace LibraryCodeGroupProject
             Console.WriteLine("3. View Books");
             Console.WriteLine("4. Back to Main Menu");
             string bookChoice = Console.ReadLine();
-            // Handle book menu choices here
+             
+            switch (bookChoice) 
+                {
+                case "1":
+                    // Handle add book logic here
+                    break;
+                case "2":
+                    // Handle remove book logic here
+                    break;
+                case "3":
+                    // Handle view books logic here
+                    break;
+                case "4":
+                    DisplayMenu();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
         }
+        // Here we put all our diffrent menu options for Books
         private void ShowCustomerMenu()
         {
             Console.WriteLine("Customers Menu:");
@@ -70,9 +92,29 @@ namespace LibraryCodeGroupProject
             Console.WriteLine("3. View Customers");
             Console.WriteLine("4. Back to Main Menu");
             string customerChoice = Console.ReadLine();
-            // Handle customer menu choices here
+            
+            switch (customerChoice)
+            {
+                case "1":
+                    // Handle add customer logic here
+                    break;
+                case "2":
+                    // Handle remove customer logic here
+                    break;
+                case "3":
+                    // Handle view customers logic here
+                    break;
+                case "4":
+                    DisplayMenu();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
 
         }
+        // Here we put all our diffrent menu options for Loans
+
         private void ShowLoanMenu()
         {
             Console.WriteLine("Loans Menu:");
@@ -81,9 +123,27 @@ namespace LibraryCodeGroupProject
             Console.WriteLine("3. View Loans");
             Console.WriteLine("4. Back to Main Menu");
             string loanChoice = Console.ReadLine();
-            // Handle loan menu choices here
+            
+            switch(loanChoice) 
+                {
+                case "1":
+                    // Handle add loan logic here
+                    break;
+                case "2":
+                    // Handle remove loan logic here
+                    break;
+                case "3":
+                    // Handle view loans logic here
+                    break;
+                case "4":
+                    DisplayMenu();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
         }
-
+        // Here we put all our diffrent menu options for Loans
 
             // Handle exit choice here
         private void ShowExit()
@@ -95,3 +155,6 @@ namespace LibraryCodeGroupProject
 
 // The menu file is not complete. Now i going to do the logic in the LibraryLogic file and then come back to the menu to connect it to the logic. I want to do this because i want to have a clear separation between the menu and the logic. This way we can easily change the menu without affecting the logic and vice versa.
 // Please add your opinons in the menu file.
+
+
+// Jag har börjat bygga ut så vi har en liknade logic i våra filer. Ni kan ändra och lägga till det ni känner för. 
--- LibraryCodeGroupProject/Program.cs
+++ LibraryCodeGroupProject/Program.cs
@@ -1,10 +1,12 @@
-﻿//Hej! -Mattis
-//hej ! andreas
-//Hej, äntligen är Ellinor här också :)
+﻿using LibraryCodeGroupProject;
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Menu menu = new Menu();
+        menu.DisplayMenu();
     }
 }
+// Now the program is complete and we can run it. We will see the main menu with options for Books, Customers, Loans and Exit. We can select any of the options to see the corresponding menu. We will add more logic and functionality to the program as we go along. We will also add some error handling and validation to make sure the program runs smoothly.
+// but we do not get any output yet, we will add some logic to the menu and library logic classes to make sure we can see some output when we run the program. We will also add some sample data to the library logic class to make sure we have something to display when we select the options in the menu. We will continue to work on this project and add more functionality as we go along.
